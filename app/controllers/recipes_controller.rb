cf299b9d4bced6b0fe17240a120fe34ed9678e2b
keithknox <keithwknox@gmail.com>
2015-06-04 13:24:51 -0400

    added buttons yo

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -29,12 +29,13 @@ class RecipesController < ApplicationController
 
   def create
   @recipe = Recipe.new(recipe_params)
-
     if @recipe.save
       redirect_to @recipe
     else
       render 'new'
     end
+    
+
   end
 
   def update
